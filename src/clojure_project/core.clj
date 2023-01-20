feaecf5dda7ab1929ad2a523c654ac1c8a2c7b81
Ekaterina Pidzhakova <imyakotoroenezanyato@gmail.com>
2023-01-20 20:38:46 +0700

    Added the symbol * to the path that gets all elements on the axis.

--- src/clojure_project/core.clj
+++ src/clojure_project/core.clj
@@ -22,6 +22,7 @@
 (defn tag [name & values]
   (concat (list ::tag name) values))
 
+; Document to string
 (defmulti to-str (fn [expr] (get-expr-type expr)))
 (defmethod to-str ::value [expr] (str expr))
 (defmethod to-str ::name [expr] (str (expr-value expr ::name)))
@@ -40,7 +41,7 @@
              (tag (nm :from) "Jani")
              (tag (nm :heading) "Reminder")
              (tag (nm :body) "Don't forget me this weekend!")))
-
+; Path
 (defn path [& values]
    values)
 
@@ -54,27 +55,35 @@
     (expr-value (first (filter #(legal-expr? % ::name) expr)) ::name)
     ""))
 
+; Check if expression satisfies node
+; The node is the element of the path
+(defmulti apply-node (fn [expr node] node))
+(defmethod apply-node :* [expr node] (legal-expr? expr ::tag))
+(defmethod apply-node :default  [expr node] (= node (tag-name expr)))
+
+; Get the content of the document that satisfies path
 (defn apply-path [expr path]
   (if (legal-expr? expr ::tag)
     (reduce
-      (fn [acc name]
+      (fn [acc node]
         (reduce (fn [acc val] (concat acc (tag-content val)))
           (list)
-          (filter (fn [elem] (= name (tag-name elem))) acc)))
+          (filter (fn [elem] (apply-node elem node)) acc)))
       (list expr)
       path)
     (throw (IllegalArgumentException. "Bad expression"))))
 
 (apply-path
   (tag (nm :html) (tag (nm :body)
-                  (tag (nm :div) "First layer" (tag (nm :span) "Text in first layer"))
+                  (tag (nm :div) "First layer"
+                       (tag (nm :span) "Text in first layer"))
                   (tag (nm :div) "Second layer")
                   (tag (nm :div) "Third layer"
                        (tag (nm :span) "Text 1 in third layer")
                        (tag (nm :span) "Text 2 in third layer")
                        (tag (nm :span) "Text 3 in third layer"))
                   (tag (nm :div) "Fourth layer")))
-  (path :html :body :div)
+  (path :html :body :div :*)
 )
 
 ;ok
